3c133d1018fbfae4e62708756fa6c8bbd01d2b71
JerryYiu <j816y@tamu.edu>
2017-08-14 17:03:17 +0000

    created cnn

---
+++ cnn.h
@@ -0,0 +1,13 @@
+#ifndef CNN_H
+#define CNN_H
+
+#include "ann.h"
+
+class Cnn: public Ann{
+    public:
+        inline Cnn();
+        inline ~Cnn();
+        
+};
+
+#endif
---
+++ test_cnn.cpp
@@ -0,0 +1,5 @@
+#include "cnn.h"
+
+int main(){
+    return 0;
+}
